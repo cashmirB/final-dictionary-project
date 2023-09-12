96bece284b1817230c5be6c68caa2b87c010fb9f
cashmirB <128428924+cashmirB@users.noreply.github.com>
2023-09-12 22:18:22 +0100

    added main and form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from "./logo1.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>My Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Cashmir Balajadia</footer>
+      </div>
     </div>
   );
 }
