0008839bdac9adad57f0d2c68822165682bd98f9
cashmirB <128428924+cashmirB@users.noreply.github.com>
2023-09-21 20:29:26 +0100

    added single phonetic

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,14 +3,11 @@ import React from "react";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <p>
-        <strong>Synonyms: </strong>
-        <ul className="Synonyms">
-          {props.synonyms.map(function (synonym, index) {
-            return <li key={index}>{synonym}</li>;
-          })}
-        </ul>
-      </p>
+      <ul className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
+        })}
+      </ul>
     );
   } else {
     return null;
